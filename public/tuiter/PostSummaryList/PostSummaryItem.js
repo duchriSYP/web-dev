ff2c62f7eb6e293779aa9cd9493dd9ebfca983e4
Chris Du <chrisdu@Chriss-MacBook-Pro.local>
2022-02-27 22:45:44 -0500

    a5 JavaScript sp22

--- public/tuiter/PostSummaryList/PostSummaryItem.js
+++ public/tuiter/PostSummaryList/PostSummaryItem.js
@@ -4,14 +4,14 @@ const PostSummaryItem = (post) => {
             <div class="col-10 mt-1">
                 <span class="wd-subtext">${post.topic} </span> <br />
                 <span class="fw-bold">
-                    ${post.username}
+                    ${post.userName}
                     <i class="fa-solid fa-circle-check"></i>
                     <span class="wd-subtext"> - ${post.time}</span>
                 </span> <br />
                 <span class="fw-bold">${post.title}</span>
             </div>
             <div class="col-2 mt-1">
-                <img src=${post.image} alt=${post.username} class="img-thumbnail wd-image-icons">
+                <img src=${post.image} alt=${post.userName} class="img-thumbnail wd-image-icons">
             </div>
         </div>
     `);
