8f9e93f77fac47eaab1f07f2c09c2bea09fb415f
Chris Du <chrisdu@Chriss-MacBook-Pro.local>
2022-02-27 21:00:09 -0500

    a5 JavaScript sp22

--- public/tuiter/ExploreScreen/ExploreScreen.js
+++ public/tuiter/ExploreScreen/ExploreScreen.js
@@ -5,10 +5,12 @@ import WhoToFollowList from "../WhoToFollowList/index.js";
 (function ($) {
     $('#wd-explore').append(`
         <div class="row mt-2">
-        <div class="col-2 col-md-2 col-lg-1 col-xl-2">
+        <div class="col-2 col-md-2 col-lg-1 col-xl-2 col-sm-3">
             ${NavigationSidebar()}
         </div>
-            
+        <div class="col-xxl-6 col-xl-6 col-lg-7 col-md-10 col-sm-9">
+            ${ExploreComponent()}
+        </div>
         <div class="d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4">
             ${WhoToFollowList()}
         </div>
