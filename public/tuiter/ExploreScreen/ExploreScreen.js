7a9a5c89739848c768880e463a82699be89a8e58
Chris Du <chrisdu@Chriss-MacBook-Pro.local>
2022-02-27 21:24:38 -0500

    a5 JavaScript sp22

--- public/tuiter/ExploreScreen/ExploreScreen.js
+++ public/tuiter/ExploreScreen/ExploreScreen.js
@@ -8,7 +8,9 @@ import WhoToFollowList from "../WhoToFollowList/index.js";
         <div class="col-2 col-md-2 col-lg-1 col-xl-2 col-sm-3">
             ${NavigationSidebar()}
         </div>
-
+        <div class="col-xxl-6 col-xl-6 col-lg-7 col-md-10 col-sm-9">
+            ${ExploreComponent()}
+        </div>
         <div class="d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4">
             ${WhoToFollowList()}
         </div>
