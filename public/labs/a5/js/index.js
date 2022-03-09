c9b92f3ab9d16aa8bf3823b78d2052e1de9bca4b
Chris Du <chrisdu@Chriss-MacBook-Pro.local>
2022-03-09 15:33:41 -0500

    a5 JavaScript sp22

--- public/labs/a5/js/index.js
+++ public/labs/a5/js/index.js
@@ -238,23 +238,5 @@ const init = () => {
     }
     hideBtn.click(hideHandler);
     showBtn.click(showHandler);
-
-    let gu = {
-        si: 'da',
-        cf: 'qe'
-    }
-    let uj = {
-        cf: 'do',
-        co: 'db',
-        ...gu,
-    }
-    gu = {
-        ...uj,
-        cf: 'wh',
-        co: 'nd'
-    }
-    console.log(gu.si)
-    console.log(uj.cf)
-    console.log(gu.co)
 }
 $(init);
