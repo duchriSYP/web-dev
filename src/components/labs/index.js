d0ddb2051add5cb89f5ea715cad1c34d4b9ff2f7
Chris Du <chrisdu@Chriss-MacBook-Pro.local>
2022-03-27 19:49:31 -0400

    a7 Redux sp22

--- src/components/labs/index.js
+++ src/components/labs/index.js
@@ -4,17 +4,14 @@ import Classes from "./classes";
 import Styles from "./styles";
 import ConditionalOutput from "./conditional-output";
 import TodoList from "./todo/TodoList";
+import ReduxExamples from "./redux-examples/ReduxExamples";
 
 const Labs = () => {
     return (
         <div>
             <h1>Labs</h1>
-            <Link to="/hello">
-                Hello
-          </Link> |
-            <Link to="/tuiter">
-                Tuiter
-          </Link>
+            <Link to="/hello"> Hello </Link> |
+            <Link to="/tuiter"> Tuiter</Link>
             <ConditionalOutput />
             <Styles />
             <Classes />
