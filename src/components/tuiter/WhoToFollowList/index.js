b624654fa491fb240a12f040b7f388b99443cffe
Chris Du <chrisdu@Chriss-MacBook-Pro.local>
2022-03-27 22:29:38 -0400

    a7 Redux sp22

--- src/components/tuiter/WhoToFollowList/index.js
+++ src/components/tuiter/WhoToFollowList/index.js
@@ -1,7 +1,8 @@
-import who from "./who.json";
 import WhoToFollowListItem from "./WhoToFollowListItem";
+import { useSelector } from "react-redux";
 
 const WhoToFollowList = () => {
+    const who = useSelector((state) => state);
     return (
         <ul className="list-group">
             <li className="list-group-item ">
--- src/components/tuiter/index.js
+++ src/components/tuiter/index.js
@@ -1,20 +1,28 @@
 import { Outlet } from "react-router-dom";
 import NavigationSidebar from "./NavigationSidebar";
 import "./ExploreScreen/explore.css";
+import whoReducer from "./reducers/who-reducer";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import WhoToFollowList from "./WhoToFollowList";
+
+const store = createStore(whoReducer);
 
 const Tuiter = () => {
     return (
-        <div className="row mt-2">
-            <div className="col-2 col-lg-1 col-xl-2">
-                <NavigationSidebar />
+        <Provider store={store}>
+            <div className="row mt-2">
+                <div className="col-2 col-lg-1 col-xl-2">
+                    <NavigationSidebar />
+                </div>
+                <div className="col-10 col-lg-7 col-xl-6">
+                    <Outlet />
+                </div>
+                <div className="d-none d-lg-block col-lg-4 col-xl-4">
+                    <WhoToFollowList />
+                </div>
             </div>
-            <div className="col-10 col-lg-7 col-xl-6">
-                <Outlet />
-            </div>
-            <div className="d-none d-lg-block col-lg-4 col-xl-4">
-                <h2>Who to follow</h2>
-            </div>
-        </div>
+        </Provider>
     );
 };
 export default Tuiter;
