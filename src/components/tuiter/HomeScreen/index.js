679ae92f32135ea4adadc96824911ca22f82ebff
Chris Du <chrisdu@Chriss-MacBook-Pro.local>
2022-03-27 23:07:01 -0400

    a7 Redux sp22

--- src/components/tuiter/HomeScreen/index.js
+++ src/components/tuiter/HomeScreen/index.js
@@ -1,8 +1,10 @@
+import TuitList from "../TuitList";
+
 const HomeScreen = () => {
     return (
         <div>
             <h2>Home</h2>
-            <h3>Tuit list</h3>
+            <TuitList />
         </div>
     )
 }
