7076c008615d1a2d5b04558c2185c380db3141e8
Chris Du <chrisdu@Chriss-MacBook-Pro.local>
2022-03-28 18:34:18 -0400

    a7 Redux sp22

--- src/components/tuiter/TuitList/TuitListItem.js
+++ src/components/tuiter/TuitList/TuitListItem.js
@@ -11,8 +11,8 @@ const TuitListItem = ({ tuit }) => {
         <div>
             <li className="list-group-item d-flex">
                 <div className="row">
-                    <div className="col-1">
-                        <img src={tuit['avatar-image']} className="rounded-circle wd-avatar justify-content-center" />
+                    <div className="col-1 wd-centered-col">
+                        <img src={tuit['avatar-image']} className="rounded-circle wd-avatar" />
                     </div>
                     <div className="col-11">
                         <div className="row">
