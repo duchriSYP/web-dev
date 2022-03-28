f69df8942b171def5f443798dedd48717549c3e2
Chris Du <chrisdu@Chriss-MacBook-Pro.local>
2022-03-27 21:06:16 -0400

    a7 Redux sp22

--- src/components/labs/redux-examples/todos-component.js
+++ src/components/labs/redux-examples/todos-component.js
@@ -1,8 +1,17 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 const Todos = () => {
-    const todos
-        = useSelector(state => state.todos);
+    const todos =
+        useSelector(state => state.todos);
+    const [todo, setTodo] = useState({ do: '' });
+    const dispatch = useDispatch();
+    const createTodoClickHandler = () => {
+        const action = {
+            type: 'create-todo',
+            todo
+        };
+        dispatch(action);
+    }
     return (
         <>
             <h3>Todos</h3>
@@ -15,6 +24,16 @@ const Todos = () => {
                     )
                 }
             </ul>
+            <li className="list-group-item">
+                <input
+                    onChange={todoChangeHandler}
+                    value={todo.do}
+                    className="form-control" />
+                <button onClick={createTodoClickHandler}
+                    className="btn btn-primary">
+                    Create New Todo
+                </button>
+            </li>
         </>
     );
 };
