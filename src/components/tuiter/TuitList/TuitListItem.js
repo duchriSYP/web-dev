0a363bbb35f4563ba5efef62e77d5b203d19cb1e
Chris Du <chrisdu@Chriss-MacBook-Pro.local>
2022-03-28 17:24:10 -0400

    a7 Redux sp22

--- src/components/tuiter/TuitList/TuitListItem.js
+++ src/components/tuiter/TuitList/TuitListItem.js
@@ -16,7 +16,7 @@ const TuitListItem = ({ tuit }) => {
                     </div>
                     <div className="col-10">
                         <div className="row">
-                            <span className="fw-bold">{tuit.handle}</span>
+                            <span className="fw-bold">{tuit.postedBy.username}</span>
                             {tuit.verified && <i className="ms-1 fas fa-badge-check"></i>}
                             <span className="ms-1 text-secondary">@{tuit.handle}</span>
                             <span>
