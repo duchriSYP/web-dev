2469ebb6ea6fca3db4468e3e9f52968f056b084d
Chris Du <chrisdu@Chriss-MacBook-Pro.local>
2022-03-27 21:14:57 -0400

    a7 Redux sp22

--- src/components/labs/redux-examples/index.js
+++ src/components/labs/redux-examples/index.js
@@ -13,7 +13,7 @@ const store = createStore(reducers);
 
 const ReduxExamples = () => {
     return (
-        <Provider store={store}>
+        <Provider store={store()}>
             <div>
                 <h2>Redux Examples</h2>
                 <Todos />
