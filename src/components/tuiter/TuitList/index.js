cafde152c4c0306a6d27ccd499da73390262cc57
Chris Du <chrisdu@Chriss-MacBook-Pro.local>
2022-04-04 02:48:11 -0400

    a8 REST sp22

--- src/components/tuiter/TuitList/index.js
+++ src/components/tuiter/TuitList/index.js
@@ -2,8 +2,7 @@ import React, { useEffect } from "react";
 import TuitListItem from "./TuitListItem";
 import { useDispatch, useSelector } from "react-redux";
 import * as service from '../../../services/tuits-service.js';
-import { findAllTuits } from "../../../actions/tuits-actions.js";
-//import './tuits.css';
+//import { findAllTuits } from "../../../actions/tuits-actions.js";
 
 const TuitList = () => {
     const tuits = useSelector(state => state.tuits);
